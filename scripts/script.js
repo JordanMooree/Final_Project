bb873c028c00c757abdf218aa395cd9317a02b9d
Lacurtis Bunch <lifesaver293@gmail.com>
2022-05-10 17:07:35 -0500

    blog page and other changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+let cards = document.querySelectorAll('.card')
+    
+function Search() {
+    let search_query = document.getElementById("searchbox").value;
+    for (var i = 0; i < cards.length; i++) {
+        if(cards[i].textContent.toLowerCase()
+                .includes(search_query.toLowerCase())) {
+            cards[i].classList.remove("hide");
+        } else {
+            cards[i].classList.add("hide");
+        }
+    }
+}
+//delay
+let typingTimer;               
+let typeInterval = 500;  
+
+let searchInput = document.getElementById('searchbox');
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(Search, typeInterval);
+});
+
 var cartText = document.getElementById("cart-count");
 document.querySelectorAll(".btn").forEach((item) =>
   item.addEventListener("click", (e) => {
