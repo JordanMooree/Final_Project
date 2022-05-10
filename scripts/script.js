62fd7167836b9d68f9cfdbb4ddb698b589c33a23
JordanMooree <jordanmooree1@gmail.com>
2022-05-10 16:10:34 -0500

    shopping cart done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,76 @@
 var cartText = document.getElementById("cart-count");
+var cartBtn = document.getElementById("cart");
+var dropdown = document.getElementById("dropdown");
+
+var list = document.getElementById("append-items");
+var totalAmount = document.getElementById("totalAmount");
+
+var submitPurchase = document.getElementById("purchase");
+
+// Alert user what item they added and append to list
 document.querySelectorAll(".btn").forEach((item) =>
   item.addEventListener("click", (e) => {
     var cartCount = parseInt(document.getElementById("cart-count").innerHTML);
+    var price = e.path[1].children[1].innerText.split("$")[1];
+    var path =
+      e.composedPath()[1].innerText.split("\n")[0] ||
+      e.path[1].children[0].innerText;
 
     cartCount++;
     cartText.innerHTML = cartCount.toString();
-    alert(e.path[1].children[0].innerText + " has been added to the cart.");
+    alert(path + " has been added to the cart.");
+
+    // Add to list
+    var entry = document.createElement("li");
+    entry.appendChild(document.createTextNode(path));
+
+    list.appendChild(entry);
+    entry.innerHTML += `<p>$${price}</p>`;
+    entry.innerHTML +=
+      '<button class="btn-cart"><i class="fa-solid fa-plus add"></i></button>';
+    entry.innerHTML +=
+      '<button class="btn-cart"><i class="fa-solid fa-trash delete"></i></button>';
+    entry.innerHTML += "<p>Qty: <span>1</span></p>";
+
+    // Add more items to cart by pressing '+'
+    document.querySelectorAll(".add").forEach((i) =>
+      i.addEventListener("click", (e) => {
+        var innerPrice = e.path[2].children[0].innerText.split("$")[1];
+        var newAmt = parseInt(innerPrice);
+        var newTotalAmt = newAmt + parseInt(totalAmount.innerText);
+        totalAmount.innerHTML = newTotalAmt.toString();
+        var qtyText = e.path[2].children[3].children[0].innerText;
+        var qty = parseInt(qtyText);
+        qty++;
+
+        e.path[2].children[3].children[0].innerText = qty.toString();
+      })
+    );
+    document.querySelectorAll(".delete").forEach((i) =>
+      i.addEventListener("click", (e) => {
+        // Subtract Total - (QTY * Price)
+        var curPrice = parseInt(e.path[2].children[0].innerText.split("$")[1]);
+        var qtyNum = parseInt(e.path[2].children[3].children[0].innerText);
+        var currentAmt = parseInt(totalAmount.innerText);
+        var newAmountAfterDelete = currentAmt - qtyNum * curPrice;
+        if (newAmountAfterDelete < 0) newAmountAfterDelete = 0;
+        totalAmount.innerText = newAmountAfterDelete.toString();
+        entry.remove();
+        cartCount--;
+        cartText.innerHTML = cartCount.toString();
+      })
+    );
+    // Total amount calculation (Addition, adding items to cart)
+    var itemPrice = parseInt(e.path[1].children[1].innerText.split("$")[1]);
+    var total = itemPrice + parseInt(totalAmount.innerText);
+    totalAmount.innerHTML = total.toString();
   })
 );
+
+cartBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("display");
+});
+
+submitPurchase.addEventListener("click", () => {
+  alert("Thank you for your order!");
+});
